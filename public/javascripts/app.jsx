c9c2e565009e11b2dbc15508ba5321c1f0baeb76
jchencc <jarvis_chen@yeah.net>
2015-03-31 16:49:14 +0800

    complete react test

--- public/javascripts/app.jsx
+++ public/javascripts/app.jsx
@@ -4,12 +4,33 @@
 //   - CommentForm
 
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.getJSON(this.props.url, function(data) {
+      this.setState({
+        data: data
+      });
+    }.bind(this));
+  },
+  handleCommentSubmit: function(comment) {
+    $.post('/api/comment/add', comment,
+      function(comment) {
+        console.log(comment);
+        this.state.data.push(comment);
+        this.setState({
+          data: this.state.data
+        });
+      }.bind(this)
+    );
+  },
   render: function() {
     return (
       <div className='comment-box'>
         <h1>Comments</h1>
-        <CommentList />
-        <CommentForm/>
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
@@ -17,11 +38,11 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var content = this.props.data.map(function(comment, i) {
+      return <Comment key={i} author={comment.author}>{comment.text}</Comment>;
+    });
     return (
-      <div className='comment-list'>
-        <Comment author='Peter Hunt'>This is one comment</Comment>
-        <Comment author='Jordan Walke'>This is *another* comment</Comment>
-      </div>
+      <div className='comment-list'>{content}</div>
     );
   }
 });
@@ -39,7 +60,14 @@ var Comment = React.createClass({
 
 var CommentForm = React.createClass({
   handleSubmit: function(e) {
-    console.log()
+    // console.log('submit', e);
+    console.log(this.refs.author.getDOMNode().value);
+    console.log(this.refs.text.getDOMNode().value);
+    this.props.onCommentSubmit({
+      author: this.refs.author.getDOMNode().value.trim(),
+      text: this.refs.text.getDOMNode().value.trim()
+    });
+    e.preventDefault();
   },
   render: function() {
     return (
@@ -53,7 +81,7 @@ var CommentForm = React.createClass({
 })
 
 React.render(
-  <CommentBox />,
+  <CommentBox url='/api/comment/list' />,
   document.getElementById('content')
 );
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/react/test', function(req, res) {
+  res.render('react/test', {title: 'React test'});
+});
+
+router.get('/api/comment/list', function(req, res) {
+  res.send([{
+    author: 'Peter Hunt',
+    text: 'This is one comment'
+  }, {
+    author: 'Jordan Walke',
+    text: 'This is another comment'
+  }]);
+});
+
+router.post('/api/comment/add', bodyParser.urlencoded({extended: false}), function(req, res) {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 module.exports = router;
